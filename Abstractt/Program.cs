289cacb548d0dd9079b8d5ada90dc04a1f530655
Emre Aydın <Eaydn81@outlook.com>
2022-07-21 17:07:52 +0300

    Add files via upload

---
+++ Abstractt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Abstractt
+{
+        abstract class Tugla
+        {
+            public int PaletKapasitesi { get; set; }
+            public string DeltaSirketi { get; set; }
+            //Tuglaların ortak Özellikleri’leri
+
+            public abstract void TuglaFiyatı();
+            //override edilecek abstract methodumuz
+        }
+        //Hitok Tuglası Sınıfı
+        class HitokTuglası : Tugla //Ucak Abstract Class’ından kalıtılan Class
+        {
+            //Tugla Abstract Class’ından, Class kalıtırken override ettiğimiz, gövdesini doldurduğumuz method.
+            public override void TuglaFiyatı() 
+            {
+                Console.WriteLine("Hitok Tuglasının Tane Fiyatı 20");
+                Console.ReadLine();
+            }
+
+        }
+        //Baca Tuglası Sınıfı
+        class BacaTuglası : Tugla
+        {
+            public override void TuglaFiyatı()
+            {
+                Console.WriteLine("Baca Tuglası Fiyatı Tane Fiyatı 23");
+                Console.ReadLine();
+
+            }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HitokTuglası H = new HitokTuglası();
+            H.TuglaFiyatı();
+        }
+    }
+}
